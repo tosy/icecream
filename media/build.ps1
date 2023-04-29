bc7db8eb5c5985a5737ccf48d2f973d6dfa0503e
tosy <ttoossyy@gmail.com>
2023-04-29 14:11:40 +0800

    meson

--- media/build.ps1
+++ media/build.ps1
@@ -1,7 +1,6 @@
 meson setup --prefix=E:/workspace/cpp/gstreamer-1.22.2/out `
 --default-library=shared -Dauto_features=disabled `
 -Dtools=enabled -Dbase=enabled -Dgst-plugins-base:opus=enabled `
--Dgood=enabled -Dgst-plugins-good:directsound=enabled `
 -Dbad=enabled -Dgst-plugins-bad:openh264=enabled -Dgst-plugins-bad:ipcpipeline=enabled `
--Dgst-plugins-bad:d3d11=enabled `
+-Dgst-plugins-bad:d3d11=enabled -Dgst-plugins-bad:wasapi2=enabled `
 --reconfigure buildcodecwin
